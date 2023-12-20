8a8e4f8fa824b154e6d2f3c82e254f0a1c362cea
Artem Filippow <a.filippov@qtech.ru>
2023-12-21 00:59:14 +0900

    debug

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,5 @@
 import {IToken, Token} from "~/server/models/token.model";
+import {User} from "~/server/models/user.model";
 
 export default defineEventHandler(async (event) => {
     const {authExpiration, authTokenName, authRefreshBeforeExpiration} = useRuntimeConfig(event)
@@ -6,8 +7,9 @@ export default defineEventHandler(async (event) => {
     console.log('xxxxxxxxxxxxxx')
     // @ts-ignore
     await Token.deleteExpiredTokens(authExpiration)
+    User.findOne()
     console.log('zzzzzzzzzzzzzz')
-    const token: IToken | null = await Token.findOne({access: cookies[authTokenName]}).populate(['user']);
+    const token: IToken | null = await Token.findOne({access: cookies[authTokenName]}).populate('user');
     console.log('fftttttttt', token)
     console.log(event.method, event._path)
     if (token) {
